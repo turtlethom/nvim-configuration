476d21745869f7a4f8793977cbe713e3c6dc6c3f
Thomas James <134099500+turtlethom@users.noreply.github.com>
2024-11-10 11:20:09 -0500

    Update init.lua

--- lazy-first-config/init.lua
+++ lazy-first-config/init.lua
@@ -1,1 +1,22 @@
+-- Configure path on startup
+vim.opt.runtimepath:prepend(vim.fn.stdpath("config") .. "/lazy-config")
+-- Loading Personal Keybindings
+require("config.keybindings")
 
+-- Lazy Vim Setup
+require("config.lazy")
+
+-- Setting Current Theme
+vim.cmd("set background=dark")
+vim.cmd("colorscheme monokai-nightasty")
+-- vim.cmd("colorscheme github_dark_high_contrast")
+
+
+-- Setting Line Numbers
+vim.wo.relativenumber = true
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
