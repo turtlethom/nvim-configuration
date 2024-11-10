0b80de2a12f4486075ca7a4805e2fbfe9171c04a
Thomas James <134099500+turtlethom@users.noreply.github.com>
2024-11-10 11:23:15 -0500

    Update keybindings.lua

--- lazy-first-config/lua/config/keybindings.lua
+++ lazy-first-config/lua/config/keybindings.lua
@@ -1,1 +1,11 @@
+-- MY PERSONAL REMAPS
 
+-- Global Remappings
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Movement Remappings
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "J", "5j", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "K", "5k", { noremap = true, silent = true })
