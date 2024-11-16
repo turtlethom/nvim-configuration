ec730eb7fe61a79b329fc49250f3423e9b4ac3dd
Thomas James <wjamesthomas3@gmail.com>
2024-11-16 16:06:23 -0500

    Removed util in wrong directory

--- util/util.lua
+++
@@ -1,9 +0,0 @@
-local function safe_command_output(command, fallback)
-	local handle = io.popen(command)
-	local result = handle and handle:read("*a")
-	if handle then
-		handle:close()
-	end
-
-	return (result and result ~= "") and result or fallback
-end
