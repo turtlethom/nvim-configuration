68f8050cdfaa8ecad15dc92500a746f33bf98ba9
Thomas James <134099500+turtlethom@users.noreply.github.com>
2024-11-16 13:05:17 -0500

    Delete lazy-first-config directory
    Resetting configuration

--- lazy-first-config/lua/config/plugins/monokai-nightasty.lua
+++
@@ -1,5 +0,0 @@
-return {
-  "polirritmico/monokai-nightasty.nvim",
-  lazy = false,
-  priority = 1000,
-}
--- lazy-first-config/lua/config/plugins/telescope-file-browser.lua
+++
@@ -1,5 +0,0 @@
---lazy
-return {
-	"nvim-telescope/telescope-file-browser.nvim",
-    	dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
-}
