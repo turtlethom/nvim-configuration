2c3e34199b102c0442e9b3f3078792ef128e3079
Thomas James <wjamesthomas3@gmail.com>
2024-12-19 01:00:27 -0500

    Added useful keybindings from Primeagen

--- lua/config/keybindings.lua
+++ lua/config/keybindings.lua
@@ -9,8 +9,20 @@ keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- Movement Remappings
 keymap.set("i", "jk", "<Esc>", opts)
+keymap.set("v", "jk", "<Esc>", opts)
+
+-- Handle System Clipboard Copy
+keymap.set("n", "<leader>y", "\"+y")
+keymap.set("n", "<leader>Y", "\"+Y")
+keymap.set("v", "<leader>y", "\"+y")
+
+-- Move Faster In Normal Mode
 keymap.set("n", "J", "5j", opts)
 keymap.set("n", "K", "5k", opts)
+
+-- Move Test Line By Line In Visual Mode
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Commenting Remap
 keymap.set("n", "<C-/>/", "gcc", { desc = "Comment out line(s) of code" })
@@ -47,6 +59,16 @@ keymap.set("n", "<leader>o", function()
   vim.cmd("Oil")
 end,
   { desc = "Open Oil" }) -- open Oil buffer from file edits
+
+-- Help Buffer Quit Remap
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "help",
+    callback = function()
+        -- Remap <leader>q to :q in help buffers
+        vim.api.nvim_buf_set_keymap(0, "n", "<leader>q", ":q<CR>", { noremap = true, silent = true })
+    end,
+})
+
 
 -- Fold Open/Close Remaps
 keymap.set("n", "<leader>u", "zo", { desc = "Open/unfold fold" })
