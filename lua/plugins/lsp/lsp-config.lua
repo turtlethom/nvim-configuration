06f6f65f2e269718d6b9b4326b54abbf8766498f
Thomas James <wjamesthomas3@gmail.com>
2024-12-31 21:36:08 -0500

    Reconfigured lua runtime paths, incorporated xdg-open into oil.nvim for image handling

--- lua/plugins/lsp/lsp-config.lua
+++ lua/plugins/lsp/lsp-config.lua
@@ -1,91 +1,96 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup({
-				ui = {
-					icons = {
-						package_installed = "✓",
-						package_pending = "➜",
-						package_uninstalled = "✗",
-					},
-				},
-			})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			-- Lua Language Server
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim" },
-						},
-						completion = {
-							callSnippet = "Replace",
-						},
-						workspace = {
-							-- library = vim.api.nvim_get_runtime_file("", true), -- Load Neovim runtime files
-						},
-						telemetry = {
-							enable = false,
-						},
-					},
-				},
-			})
+      -- Lua Language Server
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+            completion = {
+              callSnippet = "Replace",
+            },
+            workspace = {
+              library = {
+                -- vim.api.nvim_get_runtime_file("", true), -- Load Neovim runtime files
+                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                [vim.fn.stdpath("config") .. "/lua"] = true,
+                [vim.fn.stdpath("data") .. "/lazy"] = true,
+              },
+            },
+            telemetry = {
+              enable = false,
+            },
+          },
+        },
+      })
 
-			-- TypeScript Language Server
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
+      -- TypeScript Language Server
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+      })
 
-			-- Python (pylsp) Server
-			-- lspconfig.pyright.setup({
-			--      capabilities = capabilities,
-			-- })
+      -- Python (pylsp) Server
+      -- lspconfig.pyright.setup({
+      --      capabilities = capabilities,
+      -- })
 
-      -- Python (Jedi) Server
+      -- Python (Jedi) Language Server
       lspconfig.jedi_language_server.setup({})
-			-- Svelte Language Server
-			lspconfig.svelte.setup({
-				capabilities = capabilities,
-				on_attack = function(client, _)
-					vim.api.nvim_create_autocmd("BufWritePost", {
-						pattern = { "*.js", "*.ts" },
-						callback = function(ctx)
-							client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
-						end,
-					})
-				end,
-			})
+      -- Svelte Language Server
+      lspconfig.svelte.setup({
+        capabilities = capabilities,
+        on_attack = function(client, _)
+          vim.api.nvim_create_autocmd("BufWritePost", {
+            pattern = { "*.js", "*.ts" },
+            callback = function(ctx)
+              client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+            end,
+          })
+        end,
+      })
 
-			-- Powershell Language Server
-			lspconfig.powershell_es.setup({
-				capabilities = capabilities,
-				filetypes = { "ps1", "psm1", "psd1" },
-				bundle_path = "~/.local/share/nvim/mason/packages/powershell-editor-services",
-				settings = { powershell = { codeFormatting = { Preset = "OTBS" } } },
-				init_options = {
-					enableProfileLoading = false,
-				},
-			})
+      -- Powershell Language Server
+      lspconfig.powershell_es.setup({
+        capabilities = capabilities,
+        filetypes = { "ps1", "psm1", "psd1" },
+        bundle_path = "~/.local/share/nvim/mason/packages/powershell-editor-services",
+        settings = { powershell = { codeFormatting = { Preset = "OTBS" } } },
+        init_options = {
+          enableProfileLoading = false,
+        },
+      })
 
-			vim.keymap.set("n", "<leader>gh", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { noremap = true, silent = true })
-		end,
-	},
+      vim.keymap.set("n", "<leader>gh", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { noremap = true, silent = true })
+    end,
+  },
 }
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,17 +2,43 @@ return {
 	"stevearc/oil.nvim",
 	dependencies = { "echasnovski/mini.icons", version = false },
 	config = function()
-		require("oil").setup({
+    local oil = require("oil")
+    local imageExts = {
+      "png",
+      "svg",
+      "jpg",
+      "jpeg",
+      "gif",
+      "bmp",
+      "webp",
+    }
+    -- Handle selection of images and all other files
+    local function custom_select()
+      local entry = oil.get_cursor_entry()
+      if entry and entry.type == 'file' then
+        local ext = entry.name:match("^.+%.([a-zA-Z0-9]+)$")
+        if ext and vim.tbl_contains(imageExts, ext:lower()) then
+          -- Use "xdg-open" to handle images
+          vim.fn.jobstart({ "xdg-open", oil.get_current_dir() .. entry.name }, { detach = true })
+          return
+        end
+      end
+      -- Select other files normally with oil.nvim
+      oil.select()
+    end
+    oil.setup({
 			skip_confirm_for_simple_edits = true,
 			columns = { "icon" },
 			keymaps = {
+				["g?"] = { "actions.show_help", mode = "n" },
+				["<CR>"] = custom_select,
 				["<C-h>"] = false,
 				["<M-h>"] = "actions.select_split",
-        ["g."] = { "actions.toggle_hidden", mode = "n" },
+				["g."] = { "actions.toggle_hidden", mode = "n" },
 				["q"] = "actions.close",
 				["<leader>q"] = function()
-					require("oil").close()
-          vim.cmd("q!")
+					oil.close()
+					vim.cmd("q!")
 					-- vim.cmd("Dashboard")
 				end,
 			},
