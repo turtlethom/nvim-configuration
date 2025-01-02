f72117e51721583be812abaac600d6e644a30990
Thomas James <wjamesthomas3@gmail.com>
2025-01-02 18:45:45 -0500

    Added <leader>q for neovim git integration

--- lua/config/plugkeys.lua
+++ lua/config/plugkeys.lua
@@ -3,27 +3,20 @@ local opts = { noremap = true, silent = true }
 
 -- Opening Oil.nvim
 keymap.set("n", "<leader>o", function()
-  vim.cmd("Oil")
-end,
-  { desc = "Open Oil.nvim" }
-)
+	vim.cmd("Oil")
+end, { desc = "Open Oil.nvim" })
 
 -- Help Buffer Quit Remap
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "help",
-    callback = function()
-        -- Remap <leader>q to :q in help buffers
-        vim.api.nvim_buf_set_keymap(0, "n", "<leader>q", ":q<CR>", { noremap = true, silent = true })
-    end,
+	pattern = { "help", "fugitive*" },
+	callback = function()
+		-- Remap <leader>q to :q in help buffers
+		vim.api.nvim_buf_set_keymap(0, "n", "<leader>q", ":q<CR>", opts)
+	end,
 })
 
 -- Handle Bufferline Tab Navigation
 -- Create a function to map keys dynamically for bufferline tab navigation (From 1-9)
 for i = 1, 9 do
-  vim.api.nvim_set_keymap(
-    'n',
-    '<leader>t' .. i,
-    ':lua require("bufferline").go_to(' .. i .. ', true)<CR>',
-    { noremap = true, silent = true }
-  )
+	vim.api.nvim_set_keymap("n", "<leader>t" .. i, ':lua require("bufferline").go_to(' .. i .. ", true)<CR>", opts)
 end
