dec93c835871a92aa28fbe4edbbf8e56a20f3b58
Thomas James <wjamesthomas3@gmail.com>
2025-01-06 22:27:04 -0500

    Added configuration to nvim-notify in noice.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,29 +1,29 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Load plugins using Lazy.nvim
 require("lazy").setup({
-	spec = {
-		require("plugins.colorizer"),
-		require("plugins.tmux-navigator"),
-		require("plugins.treesitter"),
-		require("plugins.plenary"),
+  spec = {
+    require("plugins.colorizer"),
+    require("plugins.plenary"),
+    require("plugins.tmux-navigator"),
+    require("plugins.treesitter"),
     require("plugins.noice"),
-		require("plugins.telescope"),
-		require("plugins.dressing"),
-		require("plugins.lualine"),
-		require("plugins.themes.sonokai-theme"),
-		require("plugins.oil"),
+    require("plugins.telescope"),
+    require("plugins.dressing"),
+    require("plugins.lualine"),
+    require("plugins.themes.sonokai-theme"),
+    require("plugins.oil"),
     require("plugins.git-integration"),
-		require("plugins.todo-comments"),
-		require("plugins.bufferline"),
-		{ import = "plugins.lsp" },
-	},
-	-- install = { colorscheme = { "habamax" } },
-	checker = { enabled = true }, -- Auto-check for updates
+    require("plugins.todo-comments"),
+    require("plugins.bufferline"),
+    { import = "plugins.lsp" },
+  },
+  -- install = { colorscheme = { "habamax" } },
+  checker = { enabled = true }, -- Auto-check for updates
 })
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -5,11 +5,16 @@ return {
     -- add any options here
   },
   dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
+    {
+      "rcarriga/nvim-notify",
+      config = function()
+        vim.notify = require("notify")
+        require("notify").setup({
+          stages = "fade",
+          timeout = 1000,
+        })
+      end,
+    },
   },
 }
