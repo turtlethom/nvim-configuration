875823ea1717fb301a59816921684e6f8d679b9d
Thomas James <wjamesthomas3@gmail.com>
2025-01-10 17:58:21 -0500

    Fixed typos in cmp, added cmp-cmdline

--- lua/plugins/lsp/cmp.lua
+++ lua/plugins/lsp/cmp.lua
@@ -1,67 +1,86 @@
 return {
-	{
-		"hrsh7th/cmp-nvim-lsp",
-		dependencies = {
-			"onsails/lspkind.nvim", -- vs-code like pictograms
-		},
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-	},
+  {
+    "hrsh7th/cmp-nvim-lsp",
+    dependencies = {
+      "onsails/lspkind.nvim", -- vs-code like pictograms
+    },
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+  },
   {
     "hrsh7th/cmp-path",
   },
-	{
-		"hrsh7th/nvim-cmp",
+  {
+    "hrsh7th/cmp-cmdline",
+  },
+  {
+    "hrsh7th/nvim-cmp",
     dependencies = {
       "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path"
+      "hrsh7th/cmp-path",
     },
-		event = "InsertEnter",
-		config = function()
-			-- Set up nvim-cmp.
-			local cmp = require("cmp")
-			local lspkind = require("lspkind")
-			require("luasnip.loaders.from_vscode").lazy_load()
+    event = "InsertEnter",
+    config = function()
+      -- Set up nvim-cmp.
+      local cmp = require("cmp")
+      local lspkind = require("lspkind")
+      require("luasnip.loaders.from_vscode").lazy_load()
 
-			cmp.setup({
-				completion = {
-					completeopt = "menu,menuone,preview,noselect",
-				},
-				snippet = {
-					-- REQUIRED - you must specify a snippet engine
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<Tab>"] = cmp.mapping.select_next_item(),
-					["<S-Tab>"] = cmp.mapping.select_prev_item(),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" }, -- snippets
-					{ name = "buffer" }, -- text within buffer
-					{ name = "path" }, -- file system paths
-				}),
-				formatting = {
-					format = lspkind.cmp_format({
-						maxwidth = 50,
-						elipsis_char = "...",
-					}),
-				},
-			})
-		end,
-	},
+      cmp.setup({
+        completion = {
+          completeopt = "menu,menuone,preview,noselect",
+        },
+        snippet = {
+          -- REQUIRED - you must specify a snippet engine
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" }, -- snippets
+          { name = "buffer" }, -- text within buffer
+          { name = "path" }, -- file system paths
+        }),
+        formatting = {
+          format = lspkind.cmp_format({
+            maxwidth = 50,
+            ellipsis_char = "...",
+          }),
+        },
+      })
+      -- Set up for '/' (search mode)
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+
+      -- Set up for ':' (command-line mode)
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "cmdline" }, -- Place cmdline first for better command completion
+          { name = "path" },
+        }),
+      })
+    end,
+  },
 }
