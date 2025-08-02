694b7a0655e6c542ba74175f4efddcd8acdaf63b
Thomas James <wjamesthomas3@gmail.com>
2025-08-02 02:25:39 -0400

    tweaks to none-ls

--- lua/plugins/lsp/none-ls.lua
+++ lua/plugins/lsp/none-ls.lua
@@ -1,34 +1,36 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
-		local prettier_rules = {
-			extra_args = {
-				"--print-width",
-				"1000", -- Large value to avoid wrapping
-				"--tab-width",
-				"2", -- Default indentation width
-				"--use-tabs",
-				"false", -- Ensure spaces are used
-				"--single-quote",
-				"false",
-				"--html-whitespace-sensitivity",
-				"ignore",
-				"--prose-wrap",
-				"never",
-			},
-		}
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier.with(prettier_rules),
-				null_ls.builtins.completion.spell,
-        null_ls.builtins.formatting.shfmt
-			},
-		})
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
+    local prettier_rules = {
+      extra_args = {
+        "--print-width",
+        "1000", -- Large value to avoid wrapping
+        "--tab-width",
+        "2", -- Default indentation width
+        "--use-tabs",
+        "false", -- Ensure spaces are used
+        "--single-quote",
+        "false",
+        "--html-whitespace-sensitivity",
+        "ignore",
+        "--prose-wrap",
+        "never",
+      },
+    }
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier.with(prettier_rules),
+        null_ls.builtins.completion.spell,
+        null_ls.builtins.formatting.shfmt,
+        null_ls.builtins.formatting.gofumpt,
+        null_ls.builtins.formatting.goimports_reviser,
+      },
+    })
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
